85b8686435bc57c6927a2408a19a3794d5475eac
Chao Wang <35211026+cwang118@users.noreply.github.com>
2020-05-23 23:22:42 -0500

    Add files via upload

---
+++ SEC/SEC10Q/spiders/SEC10Q.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from scrapy.spider import BaseSpider
+from scrapy.http import Request
+from SEC10Q.items import SecScrapingItem
+import os
+import pandas as pd
+import re
+
+os.chdir("D:/SEC10Q/data/")
+SP500 = pd.read_excel('SP500.xlsx',converters={'CIK':str})\
+#.iloc[[5,27,151,182,340,453,459]]
+SP500 = pd.DataFrame(SP500.values, columns = list(SP500.columns))
+CIK = SP500['CIK']
+TIC = SP500['Ticker symbol']
+
+class Sec10qSpider(BaseSpider):
+    name = 'SEC10Q'
+    start_urls = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=' + CIK[i] + '&type=10-q&dateb=&owner=include&count=100' for i in range(len(CIK))]
+    MapCIK = dict(zip(CIK,TIC))
+#    start_urls = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0000718877&type=10-q&dateb=&owner=include&count=100']
+    def parse(self, response):
+        node_list = response.xpath("//tr/td[@nowrap='nowrap'][1]")
+        for node in node_list:
+            yield Request(str("https://www.sec.gov" + node.xpath("./following::td//a[1]/@href").extract_first()), callback=self.parseSecondLevel)
+    def parseSecondLevel(self, response):
+        item = SecScrapingItem()
+        if ".htm" in response.xpath("//tr/td[@scope='row'][3]/a/text()").extract_first():
+            #    Company name
+            item['FirmName'] = re.search(r"(.*?)\ \(",response.xpath("//span[@class='companyName'][1]/text()").extract()[0]).group(1)
+            #    CIK
+            CIK_list = response.xpath("//span[@class='companyName']/a/text()").extract()
+            for cik in CIK_list:
+                if cik[:10] in CIK.values:
+                    item['FirmCIK'] = cik[:10]
+            #    Form type
+            item['FormType'] = response.xpath("//tr/td[@scope='row'][4]/text()").extract_first()
+            #    Date
+            item['FormDate'] = response.xpath("//div[@class='formGrouping'][1]/div[@class='info'][1]/text()").extract_first()
+            #    Form Doc
+            item['FormDoc'] = str("https://www.sec.gov" + response.xpath("//tr/td[@scope='row'][3]/a/@href").extract_first())
+            #   Form Period
+            item['FormPeriod'] = response.xpath("//div[@class='formGrouping'][2]/div[@class='info'][1]/text()").extract_first()
+            #    Firm TIC
+            item['FirmTIC'] = self.MapCIK[str(item['FirmCIK'])]
+            yield item
+
+
+
